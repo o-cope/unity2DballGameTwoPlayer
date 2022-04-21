6fae4fdb1553923dd348f36a7cb14a19a6fa7831
cope <ocollinscope@gmail.com>
2022-04-21 19:31:26 +0100

    added current point in game

---
+++ ballGame/ballGame/Assets/Scripts/Player Scripts/RightPlayerMove.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightPlayerMove : MonoBehaviour
+{
+    #region Public Variables
+    #endregion
+    #region Inspector Variables
+    [SerializeField] private float moveSpeed;
+    #endregion
+    #region Private Variable
+    private float yInput;
+    #endregion
+    #region Components
+    Rigidbody2D rb;
+    #endregion
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    } //Onstartup executes
+
+    private void Update()
+    {
+        yInput = Input.GetAxis("PlayerRight");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0, yInput * moveSpeed);
+    }
+
+}
