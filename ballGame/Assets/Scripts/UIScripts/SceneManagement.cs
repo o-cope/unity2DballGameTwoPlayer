a21156b9a2793bb8731ed22f01b0b61ccd48dce2
cope <ocollinscope@gmail.com>
2022-04-22 12:26:42 +0100

    added pauseMenu, mainMenu and quit option

--- ballGame/Assets/Scripts/UIScripts/SceneManagement.cs
+++ ballGame/Assets/Scripts/UIScripts/SceneManagement.cs
@@ -8,6 +8,7 @@ public class SceneManagement : MonoBehaviour
     #region Public Variables
     #endregion
     #region Inspector Variables
+    [SerializeField] private GameObject pauseMenu;
     #endregion
     #region Private Variables
     #endregion
@@ -15,10 +16,48 @@ public class SceneManagement : MonoBehaviour
     #endregion
 
 
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SetPause();
+        }
+    }
+
     #region Methods
     public void ResetCurrentScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    public void SetPause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumePlay()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void LoadSceneMainMenu(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+        Time.timeScale = 1f;
+    }
+
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
     #endregion
 }
